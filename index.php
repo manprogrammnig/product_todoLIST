1463de9011b547d36e40b69fe313e4d399871c49
Franklin <201701718g@utea.edu.pe>
2021-05-26 19:50:08 -0500

    creando el login y haciendo estilos en css

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>TodoList</title>
 </head>
 
 <body>
-    <h1 style="text-align:left;float:left;">
-        LOGIN
-    </h1>
-    <h2> ingrese contraseña</h2>
 
+    <form action="" method="post">
+        <h1>Sistema de Login</h1> <br>
+
+        <label for="">CORREO ELECTRONICO</label>
+        <input type="text" name="usuario" placeholder="COrreo electronico" required>
+        <label for="">CONTRASEÑA</label>
+        <input type="password" name="contraseña" placeholder="Contraseña" required>
+        <button type="submit">LOGIN</button>
+        <p> <a href=>REGISTRATE</a></p>
+
+    </form>
 </body>
 
 </html>
